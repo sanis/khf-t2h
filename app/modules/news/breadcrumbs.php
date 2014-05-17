1679be0cd3ae3df198df9d1274923ccb75da9ff1
Justinas Bolys <justinas.bolys@gmail.com>
2014-05-17 14:08:00 +0300

    Cleanup before starting to use this

--- app/modules/news/breadcrumbs.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// TODO: fix
-Breadcrumbs::register('admin.group.edit', function($breadcrumbs) {
-    $breadcrumbs->parent('admin.group.list');
-    $breadcrumbs->push('Edit user group', route('admin.group.edit'));
-});
