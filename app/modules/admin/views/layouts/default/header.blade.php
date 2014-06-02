638a0b60bbca79da21fd2e4deb3e4d41ff44203d
Justinas Bolys <justinas.bolys@gmail.com>
2014-06-02 22:38:43 +0300

    Remove fake things in design

--- app/modules/admin/views/layouts/default/header.blade.php
+++ app/modules/admin/views/layouts/default/header.blade.php
@@ -1,6 +1,5 @@
 <header class="navbar navbar-inverse navbar-fixed-top" role="banner">
     <a id="leftmenu-trigger" class="tooltips" data-toggle="tooltip" data-placement="right" title="{{ trans('admin::dashboard.toggle_sidebar') }}"></a>
-    <a id="rightmenu-trigger" class="tooltips" data-toggle="tooltip" data-placement="left" title="{{ trans('admin::dashboard.toggle_infobar') }}"></a>
 
     <div class="navbar-header pull-left">
         <a class="navbar-brand" href="{{ URL::route('admin.dashboard') }}"></a>
@@ -19,85 +18,12 @@
                 <li class="userlinks">
                     <ul class="dropdown-menu">
                         <li><a href="{{ URL::route('admin.account') }}">{{ trans('admin::dashboard.account') }} <i class="pull-right fa fa-cog"></i></a></li>
-                        <li><a href="http://vcs.sanis.lt/kursiniai/vukhf/wikis/home">Help <i class="pull-right fa fa-question-circle"></i></a></li>
+                        <li><a href="https://github.com/sanis/khf-t2h/wiki">Help <i class="pull-right fa fa-question-circle"></i></a></li>
                         <li class="divider"></li>
                         <li><a href="{{ URL::route('admin.logout') }}" class="text-right">{{ trans('admin::login.log_out') }}</a></li>
                     </ul>
                 </li>
             </ul>
         </li>
-
-        <li class="dropdown">
-            <a href="#" class="hasnotifications dropdown-toggle" data-toggle='dropdown' id="woo"><i class="fa fa-bell"></i><span class="badge">3</span></a>
-            <ul class="dropdown-menu notifications arrow" id="woo2">
-                <li class="dd-header">
-                    <span>You have 3 new notification(s)</span>
-                    <span><a href="#">Mark all Seen</a></span>
-                </li>
-                <div class="scrollthis">
-                    <li>
-                        <a href="#" class="notification-user active">
-                            <span class="time">4 mins</span>
-                            <i class="fa fa-user"></i>
-                            <span class="msg">New user Registered. </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="notification-danger active">
-                            <span class="time">20 mins</span>
-                            <i class="fa fa-bolt"></i>
-                            <span class="msg">CPU at 92% on server#3! </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="notification-success active">
-                            <span class="time">1 hr</span>
-                            <i class="fa fa-check"></i>
-                            <span class="msg">Server#1 is live. </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="notification-warning">
-                            <span class="time">2 hrs</span>
-                            <i class="fa fa-exclamation-triangle"></i>
-                            <span class="msg">Database overloaded. </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="notification-order">
-                            <span class="time">10 hrs</span>
-                            <i class="fa fa-shopping-cart"></i>
-                            <span class="msg">New order received. </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="notification-failure">
-                            <span class="time">12 hrs</span>
-                            <i class="fa fa-times-circle"></i>
-                            <span class="msg">Application error!</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="notification-fix">
-                            <span class="time">12 hrs</span>
-                            <i class="fa fa-wrench"></i>
-                            <span class="msg">Installation Succeeded.</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="notification-success">
-                            <span class="time">18 hrs</span>
-                            <i class="fa fa-check"></i>
-                            <span class="msg">Account Created. </span>
-                        </a>
-                    </li>
-                </div>
-                <li class="dd-footer"><a href="#">View All Notifications</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="#" id="headerbardropdown"><span><i class="fa fa-level-down"></i></span></a>
-        </li>
-
     </ul>
 </header>
